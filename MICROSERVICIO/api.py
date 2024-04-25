72d0887df2d8e78532e724efd62c10adcb4e2313
cesarmartinez31 <147571955+cesarmartinez31@users.noreply.github.com>
2024-04-25 11:06:10 -0500

    Update api.py

--- MICROSERVICIO/api.py
+++ MICROSERVICIO/api.py
@@ -1,46 +1,78 @@
 #!/usr/bin/python
-
 from flask import Flask
 from flask_restx import Api, Resource, fields
+import pandas as pd
 import joblib
-from m09_model_deployment import predict_proba
+from m09_model_deployment import predict_price
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes and origins
+CORS(app)  # Habilitar CORS para todas las rutas y orígenes
 
 api = Api(
     app, 
     version='1.0', 
-    title='Phishing Prediction API',
-    description='Phishing Prediction API')
+    title='Used Car Price Prediction API',
+    description='API to predict the price of used cars')
 
 ns = api.namespace('predict', 
-     description='Phishing Classifier')
-   
+     description='Used Car Price Predictor')
+
 parser = api.parser()
 
 parser.add_argument(
-    'URL', 
+    'Year', 
+    type=int, 
+    required=True, 
+    help='Year of the car', 
+    location='args')
+parser.add_argument(
+    'Mileage', 
+    type=float, 
+    required=True, 
+    help='Mileage of the car', 
+    location='args')
+parser.add_argument(
+    'State', 
     type=str, 
     required=True, 
-    help='URL to be analyzed', 
+    help='State of the car', 
+    location='args')
+parser.add_argument(
+    'Make', 
+    type=str, 
+    required=True, 
+    help='Make of the car', 
+    location='args')
+parser.add_argument(
+    'Model', 
+    type=str, 
+    required=True, 
+    help='Model of the car', 
     location='args')
 
 resource_fields = api.model('Resource', {
-    'result': fields.String,
+    'result': fields.Float,
 })
 
 @ns.route('/')
-class PhishingApi(Resource):
+class UsedCarPrice(Resource):
 
     @api.doc(parser=parser)
     @api.marshal_with(resource_fields)
     def get(self):
         args = parser.parse_args()
+        year = args['Year']
+        mileage = args['Mileage']
+        state = args['State']
+        make = args['Make']
+        model = args['Model']
+        
+        # Predecir el precio del automóvil
+        price = predict_price(year, mileage, state, make, model)
         
         return {
-         "result": predict_proba(args['URL'])
+         "result": price
         }, 200
     
     
