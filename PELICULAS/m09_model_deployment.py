80048d432cf07f74533dc2b872ffd5e1adddb2ef
cesarmartinez31 <147571955+cesarmartinez31@users.noreply.github.com>
2024-05-25 17:39:03 -0500

    Update m09_model_deployment.py

--- PELICULAS/m09_model_deployment.py
+++ PELICULAS/m09_model_deployment.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python
+
 import pandas as pd
 import joblib
-from flask import Flask, request, jsonify
 
 # Cargar el modelo y el vectorizador
 model = joblib.load('model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
-# Inicializar la aplicación Flask
-app = Flask(__name__)
+def predict_rating(year, genre, director):
+    # Crear un DataFrame con los datos de entrada
+    input_data = pd.DataFrame({
+        'Year': [year],
+        'Genre': [genre],
+        'Director': [director]
+    })
 
-# Definir la ruta para la predicción
-@app.route('/predict', methods=['POST'])
-def predict_genre():
-    # Obtener los datos de entrada del cuerpo de la solicitud POST
-    data = request.json
-    
-    # Extraer la sinopsis de la película del cuerpo de la solicitud
-    plot = data['plot']
-    
-    # Preprocesar el texto de la sinopsis
-    clean_plot = preprocess_text(plot)
-    
-    # Vectorizar la sinopsis usando el vectorizador entrenado
-    X_test_dtm = vectorizer.transform([clean_plot])
-    
-    # Realizar la predicción utilizando el modelo cargado
-    predicted_probabilities = model.predict_proba(X_test_dtm)
-    
-    # Crear una lista de géneros y sus probabilidades predichas
-    genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family',
-        'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance',
-        'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
-    
-    predictions = {genre: proba for genre, proba in zip(genres, predicted_probabilities[0])}
-    
-    # Devolver las predicciones como respuesta en formato JSON
-    return jsonify(predictions)
+    # Vectorizar las características categóricas
+    input_data_encoded = vectorizer.transform(input_data)
 
-def preprocess_text(text):
-    # Aquí iría tu función preprocess_text original para limpiar y procesar el texto de la sinopsis
-    return text
-
-if __name__ == '__main__':
-    # Ejecutar la aplicación Flask en el puerto 5000
-    app.run(port=5000, debug=True)
+    # Predecir la calificación de la película
+    rating = model.predict(input_data_encoded)[0]
+    
+    return rating
