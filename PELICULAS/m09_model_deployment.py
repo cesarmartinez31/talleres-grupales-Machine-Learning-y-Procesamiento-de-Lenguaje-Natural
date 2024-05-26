fb188d03f86e44d46b4faf3c3ae54adfb517a445
cesarmartinez31 <147571955+cesarmartinez31@users.noreply.github.com>
2024-05-25 21:55:11 -0500

    Update m09_model_deployment.py

--- PELICULAS/m09_model_deployment.py
+++ PELICULAS/m09_model_deployment.py
@@ -19,6 +19,5 @@ def predict_rating(year, genre, director):
     input_data_encoded = vectorizer.transform(input_data)
 
     # Predecir la calificación de la película
-    rating = model.predict(input_data_encoded)[0]  # Tomar el primer elemento del array
-    
-    return float(rating)  # Convertir el resultado a un valor escalar float
+    rating = model.predict(input_data_encoded)[0]  # Obtener el primer elemento del array
+    return rating
