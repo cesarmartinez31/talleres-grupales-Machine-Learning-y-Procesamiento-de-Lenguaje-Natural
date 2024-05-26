2ce2c9c4dfe7f37c32b25baae9efb2187cf9ef3a
cesarmartinez31 <147571955+cesarmartinez31@users.noreply.github.com>
2024-05-25 22:49:29 -0500

    Update api.py

--- PELICULAS/api.py
+++ PELICULAS/api.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 from flask import Flask, request
 from flask_restx import Api, Resource, fields
-import pandas as pd
-import joblib
-from m09_model_deployment import predict_genres
 from flask_cors import CORS
+from m09_model_deployment import predict_genres
 
 app = Flask(__name__)
 CORS(app)
@@ -13,50 +11,34 @@ api = Api(
     app, 
     version='1.0', 
     title='Movie Genre Prediction API',
-    description='API to predict movie genres')
+    description='API to predict movie genres'
+)
 
-ns = api.namespace('predict', 
-    description='Movie Genre Predictor')
-genres = ['Comedia', 'Acción', 'Terror', 'Drama', 'Ciencia ficción', 'Romance', 'Aventura', 'Suspenso', 'Animación', 'Documental']
-# Definir el parser para los argumentos
+ns = api.namespace('predict', description='Movie Genre Predictor')
+
 parser = api.parser()
 parser.add_argument(
-    'Year', 
-    type=int, 
-    required=True, 
-    help='Release year of the movie', 
-    location='args')
-parser.add_argument(
-    'Genre', 
+    'plot', 
     type=str, 
     required=True, 
-    help='Genre of the movie', 
-    location='args',
-    choices=genres)
-parser.add_argument(
-    'Director', 
-    type=str, 
-    required=True, 
-    help='Director of the movie', 
-    location='args')
+    help='Plot of the movie', 
+    location='args'
+)
 
 resource_fields = api.model('Resource', {
-    'result': fields.List(fields.String),  # Cambiar a una lista de cadenas
+    'result': fields.List(fields.String)
 })
 
 @ns.route('/')
 class MovieGenrePredictor(Resource):
-
     @api.doc(parser=parser)
     @api.marshal_with(resource_fields)
     def get(self):
         args = parser.parse_args()
-        year = args['Year']
-        genre = args['Genre']
-        director = args['Director']
+        plot = args['plot']
         
         # Predecir los géneros de la película
-        predicted_genres = predict_genres(year, genre, director)
+        predicted_genres = predict_genres(plot)
         
         return {
             "result": predicted_genres
@@ -64,3 +46,4 @@ class MovieGenrePredictor(Resource):
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+
