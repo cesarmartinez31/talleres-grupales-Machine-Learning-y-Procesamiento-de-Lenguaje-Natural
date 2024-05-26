520c88fb95a6bf613f8c810ee37370965aa638ca
cesarmartinez31 <147571955+cesarmartinez31@users.noreply.github.com>
2024-05-25 22:47:30 -0500

    Update m09_model_deployment.py

--- PELICULAS/m09_model_deployment.py
+++ PELICULAS/m09_model_deployment.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import pandas as pd
 import joblib
 
@@ -7,24 +6,15 @@ import joblib
 model = joblib.load('model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
-# Aquí debes definir una lista con los nombres de los géneros en el mismo orden que fueron usados para entrenar el modelo
-GENRE_LABELS = ['Accion', 'Aventura', 'Comedia', 'Crimen', 'Documental', 'Drama', 'Familia', 'Fantasia', 'Historia', 'Horror', 'Musica', 'Misterio', 'Romance', 'Ciencia Ficción', 'Película de TV', 'Thriller', 'Guerra', 'Western']
 
-def predict_genres(year, genre, director):
-    # Crear un DataFrame con los datos de entrada
-    input_data = pd.DataFrame({
-        'Year': [year],
-        'Genre': [genre],
-        'Director': [director]
-    })
+# Lista de géneros en el mismo orden que fueron usados para entrenar el modelo
+GENRE_LABELS = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family',
+                'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance',
+                'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
 
-    # Vectorizar las características categóricas
-    input_data_encoded = vectorizer.transform(input_data)
-
-    # Predecir los géneros de la película
+def predict_genres(plot):
+    clean_plot = preprocess_text(plot)
+    input_data_encoded = vectorizer.transform([clean_plot])
     predicted_genres_encoded = model.predict(input_data_encoded)
-    
-    # Decodificar las predicciones
     predicted_genres = [GENRE_LABELS[i] for i, value in enumerate(predicted_genres_encoded[0]) if value == 1]
-    
     return predicted_genres
